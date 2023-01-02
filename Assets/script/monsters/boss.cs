8725b34102c64ca2adf4fee40285500d3b154d4f
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-03 00:01:02 +0800

    wuwuwuwu~~ jikjin na gonna jikjin

--- Assets/script/monsters/boss.cs
+++ Assets/script/monsters/boss.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class boss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int Speed = 5;
+    public Transform leftpoint, rightpoint;
+
+    [Header("Components")]
+    [SerializeField] protected Animator animator;
+    public Collider2D myCollider;
+
+    private float timer = 0;
+    private float tmp_timer = 0;
+    private Rigidbody2D rb;
+
+    private bool isAttack = false;
+    public bool isSkill = false;
+    public bool isAngry = false;
+    private bool isDie = false;
+    private bool isHurt = false;
+    private bool isRun = false;
+    private bool isIdle = false;
+
+    private float Xleft, Xright;
+    private float IsFaceRight = 0;
+
     void Start()
     {
-        
+        timer = Time.time;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+        Xleft = leftpoint.position.x;
+        Xright = rightpoint.position.x;
+        Destroy(leftpoint.gameObject);
+        Destroy(rightpoint.gameObject);
+        animator.SetBool("IsRun", false);
+        animator.SetBool("IsHurt", false);
+        animator.SetBool("IsAttack", false);
+        animator.SetBool("IsDead", false);
+        animator.SetBool("IsIdle", true);
+        animator.SetBool("IsAngry", false);
+        animator.SetBool("IsSkill", false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if()
         
+    }
+
+    void Movement(){
+        if(IsFaceRight == 1)
+        {
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
+            if(transform.position.x > Xright){
+                transform.localScale = new Vector3(-2,2,1);
+                IsFaceRight = 0;
+            }
+        }else if (IsFaceRight != 1){
+            rb.velocity = new Vector2(-Speed, rb.velocity.y);
+            if(transform.position.x < Xleft){
+                transform.localScale = new Vector3(2,2,1);
+                IsFaceRight = 1;
+            }
+        }
     }
 }
