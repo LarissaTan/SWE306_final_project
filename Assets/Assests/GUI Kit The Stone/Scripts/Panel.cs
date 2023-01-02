7a4df793c785873bdc5c215d6f695434a167def2
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-03 05:33:52 +0800

    bac kup

--- Assets/Assests/GUI Kit The Stone/Scripts/Panel.cs
+++ Assets/Assests/GUI Kit The Stone/Scripts/Panel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace LayerLab
 {
@@ -37,6 +38,11 @@ namespace LayerLab
         {
             Time.timeScale = 1;
             transform.Find("Popup_Settings").gameObject.SetActive(false);
+        }
+
+        public void returnMenu()
+        {
+            SceneManager.LoadScene("menu");
         }
     }
 }
--- Assets/UIPanel.cs
+++ Assets/UIPanel.cs
@@ -5,12 +5,10 @@ using UnityEngine.SceneManagement;
 public class UIPanel : MonoBehaviour
 {
     public void chongxing() {
-        //重新
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void tuichu() {
-        //退出游戏
         Application.Quit();
     }
 }
