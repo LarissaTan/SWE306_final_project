75470998c8a3b53be6a32e860e348728f1f1c18b
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-03 05:16:16 +0800

    fix bug

--- Assets/script/magic/shoot.cs
+++ Assets/script/magic/shoot.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class shoot : MonoBehaviour
 {
     private Rigidbody2D rbody;
+    private Collider2D myCollider;
     private float timer = 0;
 
 
     void Start(){
         Debug.Log("this is created");
         rbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
         timer = Time.time;
     }
 
@@ -28,7 +30,31 @@ public class shoot : MonoBehaviour
 
 
     // 碰撞检测
-    void OnCollisionEnter2D(Collision2D other){
-        Destroy(this.gameObject);
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("2333333");
+        if(collision.gameObject.tag == "eye"){
+            eye tmp = collision.gameObject.GetComponent<eye>();
+            tmp.kill();
+            Destroy(this.gameObject);
+        }
+
+        if(collision.gameObject.tag == "ghost"){
+            ghost tmp = collision.gameObject.GetComponent<ghost>();
+            tmp.kill();
+            Destroy(this.gameObject);
+        }
+
+        if(collision.gameObject.tag == "snail"){
+            Debug.Log("col enter 2d is working");
+            snails tmp = collision.gameObject.GetComponent<snails>();
+            tmp.kill();
+            Destroy(this.gameObject);
+        }
+
+        if(collision.gameObject.tag == "skeleton"){
+            skeletion tmp = collision.gameObject.GetComponent<skeletion>();
+            tmp.kill();
+            Destroy(this.gameObject);
+        }
     }
 }
