b8fe944bb6d28d1b8a1da700f05d721a9026032a
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-02 16:31:37 +0800

    add icon of keyboard

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -7,8 +7,8 @@ public class PlayerAttack : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag =="Enemy") {   //如果攻击到敌人
-            collision.transform.GetComponent<Enemy>().TakeValue(20);   //扣除敌人20血量
+        if (collision.tag =="Enemy") {  
+            collision.transform.GetComponent<Enemy>().TakeValue(20);  
         }
     }
 }
