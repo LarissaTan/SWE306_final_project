45514671c77aa3f0c347d1cabe14884394480d0b
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-03 04:23:37 +0800

    bbackuo

--- Assets/script/ui/UIController.cs
+++ Assets/script/ui/UIController.cs
@@ -10,15 +10,25 @@ public class UIController : MonoBehaviour
     public TextMeshProUGUI coin_num;
     public GameObject coin;
 
+    public TextMeshProUGUI m1_txt;
+    public GameObject m1;
+
+    public TextMeshProUGUI m2_txt;
+    public GameObject m2;
+
     // Start is called before the first frame update
     void Start()
     {
         coin_num = coin.GetComponentInChildren<TextMeshProUGUI>();
+        m1_txt = m1.GetComponentInChildren<TextMeshProUGUI>();
+        m2_txt = m2.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         coin_num.text = globe.coins.ToString();
+        m1_txt.text = globe.m1.ToString();
+        m2_txt.text = globe.m2.ToString();
     }
 }
