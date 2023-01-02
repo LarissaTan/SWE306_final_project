fd98e81bb20a132e0a702365f173d00a9631407e
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-03 04:21:01 +0800

    edit ui

--- Assets/script/core/globe.cs
+++ Assets/script/core/globe.cs
@@ -6,6 +6,8 @@ public class globe : MonoBehaviour
 {
     public static int characterIndex = 0;
     public static int coins = 0;
+    public static int m1 = 0;
+    public static int m2 = 0;
 
     // Start is called before the first frame update
     void Start()
